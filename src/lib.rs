88c6c1ed57e198c6b5f64c0b90334123a743b4df
Trard <trardone@gmail.com>
2022-10-04 17:25:36 +0300

    lib

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+mod friends_graph;
+pub use friends_graph::FriendsGraph;
+
+pub type UserId = i32;
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,4 @@
-use std::collections::HashMap;
-use std::collections::HashSet;
-
-type UserId = i32;
-
-pub struct FriendsGraph {
-    internal: HashMap<UserId, HashSet<UserId>>
-}
-
-impl FriendsGraph {
-    pub fn new() -> FriendsGraph {
-        FriendsGraph {
-            internal: HashMap::new()
-        }
-    }
-
-    pub fn add_friends_to(&mut self, id: UserId, friends: Vec<UserId>) {
-        for friend in friends {
-            self.add_friendship(id, friend);
-        }
-    }
-
-    pub fn add_friendship(&mut self, user1: UserId, user2: UserId) {
-        self.internal.entry(user1).or_insert(HashSet::new()).insert(user2);
-        self.internal.entry(user2).or_insert(HashSet::new()).insert(user1);
-    }
-
-    pub fn get_friends(&self, user: &UserId) -> Option<&HashSet<UserId>> {
-        self.internal.get(user)
-    }
-}
+use spy_bot::FriendsGraph;
 
 fn main() {
     let mut foo = FriendsGraph::new();
